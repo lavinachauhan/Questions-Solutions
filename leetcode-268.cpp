812af951ba9c0b94c21e646b4d3a092a26e7d566
Lavina Chauhan <159036025+lavinachauhan@users.noreply.github.com>
2024-07-13 01:05:05 +0530

    Add files via upload

--- leetcode-268.cpp
+++ leetcode-268.cpp
@@ -1,3 +1,6 @@
+//This question can be done with so many approaches like binary search, XOR, Negative marking 
+
+//xor approach
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -11,5 +14,50 @@ public:
             ans=ans^i;
         }
         return ans;
+    }
+};
+
+//negative marking approach
+TC->O(n);
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+       int i=0;
+       int index;
+       int ans=0;
+       bool zero=false;
+       while(i<nums.size())
+       {
+            index=abs(nums[i]);
+            if(index==0)
+            {
+                zero=true;
+            }
+            if(index>0)
+            {
+                if(nums[index-1]>0)
+                {
+                    nums[index-1]*=-1;
+                }
+            }
+            i++;
+       } 
+        if(zero==false)
+        {
+            return 0;
+        }
+        int zeroindex;
+       for(int i=0;i<nums.size();i++)
+       {
+        if(nums[i]>=0)
+            return i+1;
+
+        if(nums[i]==0)
+        {
+            zeroindex=i+1;
+        }
+       }
+       return zeroindex;
     }
 };
